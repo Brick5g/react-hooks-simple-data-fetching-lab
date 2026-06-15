9e12dc81b7fa064b84d217c78b9d88bcd4b82c39
Julian Francis <jgfrancis3@gmail.com>
2026-06-15 12:25:14 -0400

    Data fetching using react

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+const API_URL = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((r) => r.json())
+      .then((data) => setDogImage(data.message))
+      .catch((err) => {
+        console.error(err);
+        setDogImage(null);
+      });
+  }, []);
+
+  if (!dogImage) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage} alt="A Random Dog" />;
+}
+
+export default App;
+
